52dab2d3f9216d992d59128c88dddd12fd95ce3c
Andras Serfozo <subztep@gmail.com>
2018-09-18 08:48:05 +0100

    Babel 7

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,80 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')
-const uglify = require('gulp-uglify')
-const pug = require('gulp-pug')
-const babel = require('gulp-babel')
-const rename = require('gulp-rename')
-const plumber = require('gulp-plumber')
-const cssnano = require('gulp-cssnano')
+const
+	prod = process.env.NODE_ENV === 'production',
+	path = require('path'),
+	destDir = path.join(__dirname, './public_html'),
+	gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	pug = require('gulp-pug'),
+	babel = require('gulp-babel'),
+	//rename = require('gulp-rename'),
+	plumber = require('gulp-plumber'),
+	cssnano = require('gulp-cssnano'),
+	minify = require('gulp-babel-minify'),
+	preprocess = require('gulp-preprocess'),
+	gulpif = require('gulp-if')
 
-gulp.task('pug', () => {
+gulp.task('html', () => {
 	return gulp.src('pug/index.pug')
 		.pipe(plumber())
-		.pipe(pug())
-		.pipe(rename({extname: '.php'}))
-		.pipe(gulp.dest('public_html'))
+		.pipe(pug({
+			verbose: true,
+			pretty: !prod,
+			globals: [prod]
+		}))
+		.pipe(preprocess())
+		//.pipe(rename({extname: '.php'}))
+		.pipe(gulp.dest(destDir))
 })
 
-gulp.task('sass', () => {
+gulp.task('css', () => {
 	return gulp.src('scss/style.scss')
 		.pipe(plumber())
 		.pipe(sass())
-		.pipe(cssnano())
-		.pipe(gulp.dest('public_html/css'))
+		.pipe(gulpif(prod, cssnano()))
+		.pipe(gulp.dest(destDir))
 })
 
 gulp.task('js', () =>
-	gulp.src('js/main.js')
+	gulp.src([
+		//'node_modules/babel-polyfill/dist/polyfill.js',
+		'js/main.js'
+	])
 		.pipe(plumber())
-		.pipe(babel({
-			presets: ['env']
-		}))
-		.pipe(uglify())
-		.pipe(gulp.dest('public_html/js'))
+		.pipe(gulpif(prod, babel({
+			presets: [[
+				'@babel/env',
+				{
+					targets: {
+						browsers: [
+							'last 2 versions',
+							'IE >= 11'
+						]
+					},
+					useBuiltIns: 'entry'
+				}
+			]]
+		})))
+		.pipe(gulpif(prod, minify({
+			removeConsole: true
+		})))
+		.pipe(gulp.dest(destDir))
 )
 
-gulp.task('default', ['pug', 'sass', 'js'])
+gulp.task('watch', gulp.parallel('html', 'css', 'js', () => {
+	gulp.watch(
+		'pug/**/*.pug',
+		gulp.parallel('html')
+	)
+	gulp.watch(
+		'scss/**/*.scss',
+		gulp.parallel('css')
+	)
+	gulp.watch(
+		'js/**/*.js',
+		gulp.parallel('js')
+	)
+}))
 
-gulp.task('watch', ['default'], () => {
-	gulp.watch('pug/**/*.pug', ['pug'])
-	gulp.watch('scss/**/*.scss', ['sass'])
-	gulp.watch('js/**/*.js', ['js'])
-})
+gulp.task('default', gulp.parallel('html', 'css', 'js', done => {
+	done()
+}))
