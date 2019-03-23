90e1bd6d16364e89f70c0bcbd8f15baf37e82e5a
Andras Serfozo <subztep@gmail.com>
2019-03-23 10:28:48 +0000

    Upgrade Tailwind to v1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,13 @@ import NuxtConfiguration from "@nuxt/config"
 const config: NuxtConfiguration = {
   mode: "universal",
 
-  css: ["~/assets/css/tailwind.css"]
+  css: ["~/assets/styles/main.scss"],
+
+  build: {
+    splitChunks: {
+      layouts: true
+    }
+  }
 }
 
 export default config
